483be32929d951046b8726448c10c21e2d6571b7
userqiao <userqiao@163.com>
2021-02-19 14:00:14 +0800

    初始化前端小程序项目，并完成了登录页面的设计

---
+++ line-word-wx/pages/demo/demo.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<view>{{username}}</view>
+		<view v-for="one in tel">{{one}}</view>
+		<view v-if="age>=18">
+			<button @tap="signUp">我要报名</button>
+		</view>
+		<view>
+			<input type="text" v-model="address" placeholder="输入地址" />
+		</view>
+		<view>输入的内容：{{address}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username: "Scott",
+				tel: [13312345678, 15912345678],
+				age: 18,
+				address: ""
+			}
+		},
+		methods: {
+			signUp: function() {
+				uni.showToast({
+					title: "点击了报名按钮"
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ line-word-wx/pages/login/login.vue
@@ -0,0 +1,28 @@
+<template>
+	<view >
+		<view class="box-view">
+			<image src="../../static/602f45c4a4d7b.png" mode="widthFix" class="logo"></image>
+			<view class="logo-title">Line-Work在线办公系统</view>
+		</view>
+		<button class="login-btn" open-type="getUserInfo" @tap="login()">登录系统</button>
+		<view class="register-container">
+			<text>没有账号？</text>
+			<text class="register" @tap="toRegister()">立即注册</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+@import url("login.less");
+	
+</style>
